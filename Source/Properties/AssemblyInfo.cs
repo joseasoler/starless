13474570c83e10eb2031ca28a22a3e43d8cea5ab
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-03-09 15:42:02 +0100

    Add C# assembly project.

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Starless")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Starless")]
+[assembly: AssemblyCopyright("Copyright © Starless contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ACF4D02-4783-4132-9D67-577917CE9CA4")]
+
+[assembly: AssemblyVersion ("0.0.0.0")]
